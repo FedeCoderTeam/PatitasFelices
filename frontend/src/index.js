5a45b48f662d4afdfd024ca0ece467ffe9fc58f8
Nachomorales99 <nacho.morales5@gmail.com>
2023-04-11 11:26:28 -0300

    Changes in Public Folder

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,17 +2,15 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
-import {BrowserRouter} from 'react-router-dom';
-import {Provider} from 'react-redux';
-import {store} from './_redux/store';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './_redux/store';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
-    <React.StrictMode>
-        <Provider store={store}>
-            <BrowserRouter>
-                <App />
-            </BrowserRouter>
-        </Provider>
-    </React.StrictMode>
+	<Provider store={store}>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</Provider>,
 );
