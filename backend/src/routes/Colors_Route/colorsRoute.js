9cebdd4a4a088799aa0bfc5244ec31c8cb6ca4ff
Nachomorales99 <nacho.morales5@gmail.com>
2023-04-11 21:25:06 -0300

    Backend:
    -Add folder and file colors
    -Add fodler and file gender
    -Create Json Colors
    -Create Json Genders
    -Charge database
    -Delete Race model

---
+++ backend/src/routes/Colors_Route/colorsRoute.js
@@ -0,0 +1,18 @@
+const { Router } = require('express');
+const router = Router();
+const dogsColors = require('../../controllers/Color_Controllers/colorsControllers');
+
+//get colors from db
+
+router.get('/', async (req, res) => {
+	try {
+		let allColors = await dogsColors();
+
+		if (allColors.error) throw new Error(allColors.error);
+		res.status(200).json(allColors);
+	} catch (error) {
+		res.status(400).json({ error: error.message });
+	}
+});
+
+module.exports = router;
---
+++ backend/src/routes/Genders_Route/gendersRoute.js
@@ -0,0 +1,18 @@
+const { Router } = require('express');
+const router = Router();
+const dogsGenders = require('../../controllers/Gender_Controllers/genderControllers');
+
+//get genders from db
+
+router.get('/', async (req, res) => {
+	try {
+		let allGenders = await dogsGenders();
+
+		if (allGenders.error) throw new Error(allGenders.error);
+		res.status(200).json(allGenders);
+	} catch (error) {
+		res.status(400).json({ error: error.message });
+	}
+});
+
+module.exports = router;
