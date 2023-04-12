878e75504398c8dde163d3b0d807fb7c587a5646
Belen-Cufre <belen_cufre@hotmail.com>
2023-04-12 20:47:37 -0300

    Backup: Edit of index

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,6 +1,8 @@
 const { Router } = require('express');
 const dogsRoute = require('./Dogs_Routes/dogsRoute');
 const temperamentsRoute = require('./Temperaments_Routes/temperamentsRoute');
+const colorsRoute= require('./Colors_Route/colorsRoute');
+const gendersRoute= require('./Genders_Route/gendersRoute')
 const router = Router();
 
 router.use('/dogs', dogsRoute);
