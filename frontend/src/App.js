9be03763e8fb8433cdec099410bc14c1d2c760a9
FacuRecabarren <facundorecabarren10@gmail.com>
2023-04-12 12:47:51 -0300

    PatitasFelices - Backend:   - None
    - Frontend:
      - Componente Landing
      - Landing css
      - Ruta Landing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,13 +5,15 @@ import About from './views/About/About';
 import Footer from './components/Footer/Footer';
 import Nav from './components/Nav/Nav';
 import Register from './components/Register/Register';
+import Landing from './components/Landing/Landing';
 
 function App() {
   return (<>
             {/* <Header/> */}
             {/* <Nav/> */}
             <Routes>
-                <Route path={'/'} element={<Home/>} />
+                <Route path={'/'} element={<Landing/>} />
+                <Route path={'/home'} element={<Home/>} />
                 <Route path={'/about'} element={<About/>} />
                 <Route path={'/register'} element={<Register />} />
                 {/* <Route path={'/login'} element={<Login />} /> */}
