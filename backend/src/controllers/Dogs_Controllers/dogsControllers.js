e4549abb7a0cd44274ebed3e7dd05f294675fb9d
Nachomorales99 <nacho.morales5@gmail.com>
2023-04-14 20:06:40 -0300

    backend: -Add description in model Dogs and dogsControllers

--- backend/src/controllers/Dogs_Controllers/dogsControllers.js
+++ backend/src/controllers/Dogs_Controllers/dogsControllers.js
@@ -29,6 +29,7 @@ const getAllDogs = async () => {
 				weight: alldogs[i].weight,
 				castrated: alldogs[i].castrated,
 				image: alldogs[i].image,
+				description: alldogs[i].description,
 			});
 
 			let colors = await color.findAll({ where: { name: alldogs[i].colors } });
@@ -62,6 +63,7 @@ const getAllDogs = async () => {
 			weight: el.weight,
 			castrated: el.castrated,
 			image: el.image,
+			description: el.description,
 			temperaments: el.temperaments.map((t) => t.name),
 			colors: el.colors.map((c) => c.name),
 			gender: genderStr,
@@ -110,6 +112,7 @@ const getDogById = async (id) => {
 			weight: dogui.weight,
 			castrated: dogui.castrated,
 			image: dogui.image,
+			description: dogui.description,
 			temperaments: dogui.temperaments.map((t) => t.name),
 			colors: dogui.colors.map((c) => c.name),
 			gender: genderStr,
