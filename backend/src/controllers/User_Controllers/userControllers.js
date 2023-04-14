6a02244e40cf7a5c3bebba4c085020f08f4cf528
Fede-Coder <federiconieto04@gmail.com>
2023-04-14 16:51:36 -0300

    PatitasFelices - Backend:   - package.json: Added jsonwebtoken   - Controllers:     - function loginUser(...): Modified added creation session.     - function authUser(...): Added various things for authentication controls   - Routes:     - Modified route "/auth"
    - Frontend:
      - None

--- backend/src/controllers/User_Controllers/userControllers.js
+++ backend/src/controllers/User_Controllers/userControllers.js
@@ -1,6 +1,7 @@
-const { user, role } = require ('../../database/db')
+const { user, role , session} = require ('../../database/db')
 const usersJson = require ('../../Json/Users.json')
 const jwt = require('jsonwebtoken')
+const {where} = require('sequelize');
 
 const getAllUsers = async ()=> {
     let data= await user.findAll({
@@ -121,10 +122,15 @@ const loginUser = async (email, password) => {
     }
 
     if(findUser.email === email && findUser.password === password) {
-        return jwt.sign({findUser}, "MIGeMA0GCSqGSIb3DQEBAQUAA4GMADCBiAKBgHY/fLfFMx4BdEsj49b5SqdlY4Ls\n" +
+        const token = jwt.sign({findUser}, "MIGeMA0GCSqGSIb3DQEBAQUAA4GMADCBiAKBgHY/fLfFMx4BdEsj49b5SqdlY4Ls\n" +
             "cHk2Z8ui3fAPwAFQe9YtOnRFCP4A2IbmuXQgTt98HTNV8OAqsFoX9/zG1v8++WDj\n" +
             "uO+7n2tUY9pvN609L41oKYo0fmo2FVgP5xJMBzuEvHyCU7k9VgOstruEZgDOKxBE\n" +
             "1r2avOYVGbYCBIzZAgMBAAE", {expiresIn: '1h'})
+        await session.create({
+            token: token,
+            userId: findUser.id
+        })
+        return token
     } else {
         throw new Error('El email o contraseña no son validos.')
     }
@@ -132,7 +138,21 @@ const loginUser = async (email, password) => {
 
 //En progreso
 const authUser = async (token) => {
+    if(!token) throw new Error('You need token')
 
+    const findSession = await session.findOne({where: {token: token}})
+    if(!findSession) throw new Error('No exist you token')
+
+    const findUser = await user.findOne({where: {id: findSession.id}})
+    if(!findUser) throw new Error('Not authorized')
+    jwt.verify(findSession.token, "MIGeMA0GCSqGSIb3DQEBAQUAA4GMADCBiAKBgHY/fLfFMx4BdEsj49b5SqdlY4Ls\n" +
+        "cHk2Z8ui3fAPwAFQe9YtOnRFCP4A2IbmuXQgTt98HTNV8OAqsFoX9/zG1v8++WDj\n" +
+        "uO+7n2tUY9pvN609L41oKYo0fmo2FVgP5xJMBzuEvHyCU7k9VgOstruEZgDOKxBE\n" +
+        "1r2avOYVGbYCBIzZAgMBAAE", function (err) {
+        if(err) {
+            throw new Error(err)
+        }
+    })
 }
 
 
@@ -142,4 +162,5 @@ module.exports= {
     postNewUser,
     updateUser,
     loginUser,
+    authUser
 }
