e1b521257a1cdd90432aa527afdc4cac8464789d
Nachomorales99 <nacho.morales5@gmail.com>
2023-04-14 12:12:26 -0300

    backend: -Route getproductbyid -Route getdogsbyid

--- backend/src/routes/Products_Route/podructsRoute.js
+++ backend/src/routes/Products_Route/podructsRoute.js
@@ -1,6 +1,7 @@
 const { Router } = require('express');
 const router = Router();
 const getAllProducts = require('../../controllers/Product_Controllers/productControllers');
+const getProductById = require('../../controllers/Product_Controllers/productIdControllers');
 
 router.get('/', async (req, res) => {
 	try {
@@ -9,6 +10,20 @@ router.get('/', async (req, res) => {
 		if (allProducts.error) throw new Error(allProducts.error);
 
 		res.status(200).json(allProducts);
+	} catch (error) {
+		res.status(400).json({ error: error.message });
+	}
+});
+
+router.get('/:id', async (req, res) => {
+	let { id } = req.params;
+
+	try {
+		let productId = await getProductById(id);
+
+		if (productId.error) throw new Error(productId.error);
+
+		res.status(200).json(productId);
 	} catch (error) {
 		res.status(400).json({ error: error.message });
 	}
