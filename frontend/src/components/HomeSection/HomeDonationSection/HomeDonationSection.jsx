9ba7fcc195bfc607673f10326ab208678e882da2
santireca <santiagorecabarren7@gmail.com>
2023-04-19 03:26:11 -0300

    PatitasFelices - Backend:   - None
    - Frontend:
      - Home CSS remade
      - Rewritten Header message

--- frontend/src/components/HomeSection/HomeDonationSection/HomeDonationSection.jsx
+++ frontend/src/components/HomeSection/HomeDonationSection/HomeDonationSection.jsx
@@ -1,31 +1,28 @@
 import React from 'react';
-import './HomeDonationSection.css';
-import dogDonation from './images/dogDonation.png';
-import dogProducts from './images/dogProducts.png';
+import './homeDonationSection.css';
 import { Link } from 'react-router-dom';
 
 const HomeDonationSection = () => {
 	return (
 		<div className="mainContainer-HDonationS">
-			<h1>Ayudanos a ayudar</h1>
+
 			<div className="containerBoxes-HDonationS">
-				<div className="containerRight-HDonationS">
-					<img src={dogProducts} alt="dogProducts" />
-					<div className="containerBtn-HDonationS">
-						<button>
-							<Link to="/products">Ver productos</Link>
-						</button>
-					</div>
-				</div>
 				<div className="containerLeft-HDonationS">
-					<img src={dogDonation} alt="dogDonation" />
+					<Link to='/donation'>
+						<img src='https://res.cloudinary.com/dreso9ye9/image/upload/v1681880432/133833-money-donation_siju2e.gif' alt="dogDonation" className='donationImage'/>
+					</Link>
 					<div className="containerBtn-HDonationS">
-						<button>
-							<Link to="/donation">Donar</Link>
-						</button>
 					</div>
 				</div>
 			</div>
+
+			<div className='containerRight-HDonationS'>
+				<h1>Ayudanos a <span>ayudar</span></h1>
+				<h4>Tu donación les proveerá de alimento, atención médica y un hogar temporal</h4>
+				<h3>¡Incluso la ayuda más pequeña puede marcar la diferencia en sus vidas!</h3>
+				<Link to="/donation"><button className='button'>¡Donar!</button></Link>
+			</div>
+
 		</div>
 	);
 };
