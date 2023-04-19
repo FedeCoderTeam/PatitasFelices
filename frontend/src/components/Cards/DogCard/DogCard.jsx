6906fad005c010f231cdefe48e16c9ec1b7d62b4
Flornb <bohn.florencia@gmail.com>
2023-04-19 18:47:26 -0300

    conflict fixed

--- frontend/src/components/Cards/DogCard/DogCard.jsx
+++ frontend/src/components/Cards/DogCard/DogCard.jsx
@@ -14,6 +14,8 @@ import {
 } from '@mui/material';
 import {brown} from '@mui/material/colors';
 import {useNavigate} from 'react-router-dom';
+import { useDispatch } from "react-redux";
+import { setMaybeAdoptedDogs } from "../../../_redux/actions/dogsAction";
 
 
 
@@ -75,10 +77,15 @@ const DogCard = ({image, name, age, gender, size, weight, colors, temperaments, 
 }
 
 export default DogCard;
+   
+export function DialogDogsDetail(props) {
 
-export function DialogDogsDetail (props) {
-    
-    
+        const dispatch = useDispatch();
+        const handleClick = (id)=>{
+            console.log(id);
+            dispatch(setMaybeAdoptedDogs(id))
+            navigate('/form')
+        } 
     const navigate = useNavigate()
 
     const innerTheme = createTheme({
@@ -143,11 +150,10 @@ export function DialogDogsDetail (props) {
                                 {props.dog.separatedTemperaments}
                             </h3>
                         </div>
-                    </DialogContentText>
-                </DialogContent>
-                            
-                <DialogActions>
-                    <Button className= {style.formButton} variant="contained" onClick={() => navigate('/form') } autoFocus>
+                        </DialogContentText>
+                            </DialogContent>
+                            <DialogActions>
+                                <Button variant="contained" onClick={() =>{ handleClick(props.dog.id)} } autoFocus>
                         ¡Completa el formulario para adoptarlo!
                     </Button>
                 </DialogActions>        
