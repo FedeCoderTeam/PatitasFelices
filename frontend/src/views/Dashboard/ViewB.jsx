6a354607ce103e76bc7da7dba58a09be710c3366
FacuRecabarren <facundorecabarren10@gmail.com>
2023-04-22 17:04:35 -0300

    PatitasFelices - Backend:   - None
    - Frontend:
      - Añadi una propiedad mas en productControllers y productIdControllers
      - CSS de UpdateProductForm
      - Logica de UpdateProductForm, falta corregir una cosa

--- frontend/src/views/Dashboard/ViewB.jsx
+++ frontend/src/views/Dashboard/ViewB.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 import { Link } from 'react-router-dom';
+import * as productsAction from '../../_redux/actions/productsAction';
+import { useDispatch } from "react-redux";
 
 const ViewB = () => {
+
+    const dispatch = useDispatch();
+
+    const handleClick = () =>{
+        dispatch(productsAction.getProductsById(4))
+    }
+
     return (
         <div>
             <section className="containerBtns-ViewB">
@@ -11,7 +20,7 @@ const ViewB = () => {
                     </Link>
                     <Link to='./CreateDog'><button>Crear Perro</button></Link>
                     <Link to='./updateDog'><button>Editar Perro</button></Link>
-                    <Link to='./updateProduct'><button>Editar Producto</button></Link>
+                    <Link to='./updateProduct'><button onClick={handleClick}>Editar Producto</button></Link>
                 </div>
             </section>
         </div>
