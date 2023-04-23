42a980a7553b0885a91f2c63a18e1a1e560dba4b
Rodrigo Palomo <repmlm@hotmail.com>
2023-04-23 14:40:06 -0300

    botones dashboard

--- frontend/src/views/Dashboard/Admin_home/Home.jsx
+++ frontend/src/views/Dashboard/Admin_home/Home.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import style from './homeDash.module.css';
 
 const Home = () => {
 	return (
-		<div>
-			<Link to="/dashboard">
-				<button style={{ marginTop: '40px' }}>OLVERA HOME DASHBOARD</button>
-			</Link>
-			<Link to="/home">
-				<button style={{ marginTop: '40px' }}>VOLVER A HOME</button>
-			</Link>
+		<>
+			<div className={style.containerBtnNav}>
+				<Link to="/dashboard" style={{ textDecoration: 'none' }}>
+					<button className={style.btnBackHomeDash}>Home Dashboard</button>
+				</Link>
+				<Link to="/home" style={{ textDecoration: 'none' }}>
+					<button className={style.btnBackHome}>Home Principal</button>
+				</Link>
+			</div>
 			<h1>HOMEEEEEEEEEEEEEEE</h1>
-		</div>
+		</>
 	);
 };
 
