6cfc2f68372e7bb8ce6261fa1d59be854ebfbba0
Fede-Coder <federiconieto04@gmail.com>
2023-04-23 01:08:56 -0300

    PatitasFelices - Backend:   - Configured for deployment
    - Frontend:
      - Configured for deployment

--- backend/src/routes/Auths_Routes/authRoute.js
+++ backend/src/routes/Auths_Routes/authRoute.js
@@ -18,7 +18,6 @@ const router = Router()
 
 router.post('/', cors({credentials: true, origin: 'https://patitas-felices.vercel.app'}), async (req, res) => {
     const { tkn_usr } = req.cookies
-    console.log(tkn_usr)
     try {
         let result = await authUser(tkn_usr);
         res.status(200).json(result)
