b039c1c39c4bc930fff8f40b4ee3fb66920c6400
Nachomorales99 <nacho.morales5@gmail.com>
2023-04-26 02:02:09 -0300

    backend: - -Permanent users
    frontend:
    - Color icons in nav

--- frontend/src/components/Nav/Nav.jsx
+++ frontend/src/components/Nav/Nav.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Link, NavLink} from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import style from '../Nav/nav.module.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { useLocation } from 'react-router-dom';
@@ -10,41 +10,65 @@ import Avatar from '@mui/material/Avatar';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 import Typography from '@mui/material/Typography';
-import {Badge, Divider, ListItemIcon} from '@mui/material';
+import { Badge, Divider, ListItemIcon } from '@mui/material';
 import { Settings, Logout } from '@mui/icons-material';
 import { logoutUserAction } from '../../_redux/actions/authAction';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import {setOpenAction} from '../../_redux/actions/productsAction';
+import StarIcon from '@mui/icons-material/Star';
+import { setOpenAction } from '../../_redux/actions/productsAction';
+import { red } from '@mui/material/colors';
+import { grey } from '@mui/material/colors';
 
 export default function Nav() {
 	const location = useLocation();
 
-	const dispatch = useDispatch()
+	const dispatch = useDispatch();
 	const selector = useSelector((state) => state.authReducer);
-	const shoppingCart = useSelector(state => state.productsReducer.shoppingCart)
+	const shoppingCart = useSelector(
+		(state) => state.productsReducer.shoppingCart,
+	);
 
 	return (
 		<div className={style.containerNav}>
 			<div className={style.logoContainer}>
-				<Link to='/home'>
-					<img className={style.logo} src="https://res.cloudinary.com/dreso9ye9/image/upload/v1682299697/Logo_yiz4g6.png" alt="" />
+				<Link to="/home">
+					<img
+						className={style.logo}
+						src="https://res.cloudinary.com/dreso9ye9/image/upload/v1682299697/Logo_yiz4g6.png"
+						alt=""
+					/>
 				</Link>
 			</div>
 
 			<div className={style.containerLeft}>
-				<NavLink to="/home" className={`${style.link} ${location.pathname === '/home' ? style.active : ''}`}>
+				<NavLink
+					to="/home"
+					className={`${style.link} ${
+						location.pathname === '/home' ? style.active : ''
+					}`}
+				>
 					<div className={style.divNav}>
 						<i className="fa-solid fa-house"></i>
 						<p className={style.links}>Inicio</p>
 					</div>
 				</NavLink>
-				<NavLink to="/products" className={`${style.link} ${location.pathname === '/products' ? style.active : ''}`}>
+				<NavLink
+					to="/products"
+					className={`${style.link} ${
+						location.pathname === '/products' ? style.active : ''
+					}`}
+				>
 					<div className={style.divNav}>
 						<i className="fa-solid fa-bone"></i>
 						<p className={style.links}>Productos</p>
 					</div>
 				</NavLink>
-				<NavLink to="/dogs" className={`${style.link} ${location.pathname === '/dogs' ? style.active : ''}`}>
+				<NavLink
+					to="/dogs"
+					className={`${style.link} ${
+						location.pathname === '/dogs' ? style.active : ''
+					}`}
+				>
 					<div className={style.divNav}>
 						<i className="fa-solid fa-paw"></i>
 						<p className={style.links}>Perros</p>
@@ -76,8 +100,17 @@ export default function Nav() {
 				</div>
 			) : (
 				<div className={style.containerRight}>
-					<IconButton arial-label={'primary'} onClick={() => dispatch(setOpenAction())} >
-						<Badge badgeContent={shoppingCart.items.reduce((total, item) => total + item.quantity, 0)} color={'primary'} >
+					<IconButton
+						arial-label={'primary'}
+						onClick={() => dispatch(setOpenAction())}
+					>
+						<Badge
+							badgeContent={shoppingCart.items.reduce(
+								(total, item) => total + item.quantity,
+								0,
+							)}
+							color={'primary'}
+						>
 							<ShoppingCartIcon />
 						</Badge>
 					</IconButton>
@@ -103,14 +136,17 @@ export function AvatarComponent(props) {
 
 	const handleLogout = () => {
 		dispatch(logoutUserAction(props.selector.user.id));
-	}
+	};
 
 	return (
 		<>
 			<Box sx={{ flexGrow: 0 }} width={'244px'} textAlign={'end'}>
 				<Tooltip title="Mi cuenta">
 					<IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-						<Avatar alt={props.selector.user.name} src={props.selector.user.image} />
+						<Avatar
+							alt={props.selector.user.name}
+							src={props.selector.user.image}
+						/>
 					</IconButton>
 				</Tooltip>
 				<Menu
@@ -133,17 +169,32 @@ export function AvatarComponent(props) {
 						{props.selector.user.name} {props.selector.user.last}
 					</MenuItem>
 					<Divider />
+					<MenuItem
+						onClick={(event) => {
+							handleCloseUserMenu(event);
+						}}
+					>
+						<ListItemIcon>
+							<StarIcon fontSize="small" color="warning" />
+						</ListItemIcon>
+						Mis comentarios
+					</MenuItem>
 					<MenuItem onClick={handleCloseUserMenu}>
 						<ListItemIcon>
-							<Settings fontSize="small" />
+							<Settings fontSize="small" sx={{ color: grey[900] }} />
 						</ListItemIcon>
-						Settings
+						Ajustes
 					</MenuItem>
-					<MenuItem onClick={(event) => {handleCloseUserMenu(event); handleLogout()}}>
+					<MenuItem
+						onClick={(event) => {
+							handleCloseUserMenu(event);
+							handleLogout();
+						}}
+					>
 						<ListItemIcon>
-							<Logout fontSize="small" />
+							<Logout fontSize="small" sx={{ color: red[500] }} />
 						</ListItemIcon>
-						Logout
+						Salir
 					</MenuItem>
 				</Menu>
 			</Box>
