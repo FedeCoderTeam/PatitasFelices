66e7cdb1fc9acb34fc64be2b0871e89cafd65d21
Fede-Coder <federiconieto04@gmail.com>
2023-04-28 22:22:53 -0300

    PatitasFelices - Backend   - None
    - Frontend
      - PasswordReset (In progress)

--- frontend/src/components/AuthForms/PasswordReset/PasswordReset.jsx
+++ frontend/src/components/AuthForms/PasswordReset/PasswordReset.jsx
@@ -1,14 +1,18 @@
-import React, {useEffect, useRef} from 'react';
+import React, { useEffect } from 'react';
 import {Link, useNavigate, useSearchParams} from 'react-router-dom';
 import './PasswordReset.css';
 import {useDispatch, useSelector} from 'react-redux';
 import { verifyPasswordResetAction, confirmPasswordResetAction } from '../../../_redux/actions/authAction';
+import * as Yup from "yup";
+import {useFormik} from "formik";
+import {Button, CircularProgress, createTheme, TextField, ThemeProvider} from "@mui/material";
 
 const PasswordReset = () => {
 
     const [searchParams] = useSearchParams()
     const dispatch = useDispatch();
     const statusVerify = useSelector(state => state.authReducer.statusVerify)
+    const isFetching = useSelector(state => state.authReducer.isFetching)
     const navigate = useNavigate()
 
     const token = searchParams.get('token')
@@ -26,19 +30,51 @@ const PasswordReset = () => {
 
     }, [dispatch, statusVerify, navigate])
 
-    const refPass = useRef(null);
-    const refRepeatPass = useRef(null);
+    const initialValues = {
+        password: '',
+        confirmPassword: '',
+    }
 
+    const validationSchema = Yup.object({
+        password: Yup
+            .string()
+            .min(6, 'Password should be of minimum 6 characters length')
+            .max(32, 'Password should be of minimum 32 characters length')
+            .matches(/^\S+$/, 'Cannot contain spaces')
+            .required('Password is required'),
+        confirmPassword: Yup
+            .string()
+            .oneOf([Yup.ref('password'), null], 'Password must match')
+    })
 
-
-    const handleOnConfirmPasswordReset = () => {
-        if (!refPass.current.value || !refRepeatPass.current.value) return;
-        if(refPass.current.value === refRepeatPass.current.value) {
-            dispatch(confirmPasswordResetAction(token, refPass.current.value)).then(() => {
+    const handleOnSubmit = (values) => {
+        dispatch(
+            dispatch(confirmPasswordResetAction(token, values.password)).then(() => {
                 navigate('/login')
             })
+        )
+    }
+
+    const formik = useFormik({
+        initialValues: initialValues,
+        validationSchema: validationSchema,
+        onSubmit: handleOnSubmit
+    })
+
+    const theme = createTheme({
+        palette: {
+            primary: {
+                main: '#163440'
+            },
+            secondary: {
+                main: '#592519'
+            },
+            action: {
+                disabledBackground: '#244e6b',
+                disabled: '#fff'
+            }
         }
-    };
+    })
 
     return (
         <div className="mainContainer-Register" data-aos="fade-left">
@@ -50,23 +86,51 @@ const PasswordReset = () => {
                 <div className="containerInputs-Register">
 
                     <div className="contraseña">
-                        <label>Contraseña</label>
-                        <input type="password" ref={refPass} />
+                        <ThemeProvider theme={theme}>
+                            <TextField
+                                disabled={isFetching}
+                                fullWidth={true}
+                                name={'password'}
+                                label={'Password'}
+                                type={'password'}
+                                variant={'filled'}
+                                value={formik.values.password}
+                                onChange={formik.handleChange}
+                                error={formik.touched.password && Boolean(formik.errors.password)}
+                                helperText={formik.touched.password && formik.errors.password}
+                            />
+                        </ThemeProvider>
                     </div>
 
                     <div className="contraseña">
-                        <label>Contraseña</label>
-                        <input type="password" ref={refRepeatPass} />
+                        <ThemeProvider theme={theme}>
+                            <TextField
+                                disabled={isFetching}
+                                fullWidth={true}
+                                name={'confirmPassword'}
+                                label={'Confirm Password'}
+                                type={'password'}
+                                variant={'filled'}
+                                value={formik.values.confirmPassword}
+                                onChange={formik.handleChange}
+                                error={formik.touched.confirmPassword && Boolean(formik.errors.confirmPassword)}
+                                helperText={formik.touched.confirmPassword && formik.errors.confirmPassword}
+                            />
+                        </ThemeProvider>
                     </div>
 
                     <div className="containerButton-Register">
-                        <button
-                            type="submit"
-                            className="buttonRegister"
-                            onClick={handleOnConfirmPasswordReset}
-                        >
-                            Enviar
-                        </button>
+                        <ThemeProvider theme={theme}>
+                            <Button onClick={formik.handleSubmit} disabled={isFetching} fullWidth={true} type="submit" color={'secondary'} size={'large'} variant="contained" sx={{ '&:hover': { backgroundColor: '#163440' } }} >Enviar</Button>
+                            {isFetching && (<CircularProgress size={24} sx={{
+                                color: '#D9AD77',
+                                position: 'absolute',
+                                top: '50%',
+                                left: '50%',
+                                marginTop: '-12px',
+                                marginLeft: '-12px',
+                            }}></CircularProgress>)}
+                        </ThemeProvider>
                     </div>
                 </div>
 
